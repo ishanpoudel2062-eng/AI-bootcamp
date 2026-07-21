27e4f15f1f78e16cd643011effb7db768e1e6ec9
Ishan Poudel <ishan.poudel2062@gmail.com>
2026-07-21 23:29:10 +0545

    Add files via upload

---
+++ oop programming/instant_attributes.py
@@ -0,0 +1,17 @@
+class employee:
+    company="techpana"
+    salary = 41000
+    
+    
+ishan=employee() 
+ganesh=employee()
+
+ishan.salary=440000
+
+print(ishan.company)
+
+print(ganesh.company)
+
+print(ishan.salary)
+
+print(ganesh.salary)
